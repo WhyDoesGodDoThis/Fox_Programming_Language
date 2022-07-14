58f14ae40e25a72bd32d2279143ae65b34c94332
WhyDoesGodDoThis <phoenixnet.phoenix@gmail.com>
2022-07-14 16:40:00 -0400

    16 instructions now instruction dict updated, runtime next?

--- New_VM.py
+++ New_VM.py
@@ -14,20 +14,21 @@ class VM(Frame):
         self.lines = []
         self.instructions = {
             b'\x01': self.print,
-            b'\x02': self.move,
-            b'\x03': self.nul,
-            b'\x04': self.nul,
-            b'\x05': self.nul,
-            b'\x06': self.nul,
-            b'\x08': self.nul,
-            b'\x09': self.nul,
-            b'\x0a': self.nul,
-            b'\x0b': self.nul,
-            b'\x0c': self.nul,
-            b'\x0d': self.nul,
-            b'\x0e': self.nul,
-            b'\x0f': self.nul,
-            b'\x10': self.nul,
+            b'\x02': self.make,
+            b'\x03': self.move,
+            b'\x04': self.load,
+            b'\x05': self.unload,
+            b'\x06': self.set_register,
+            b'\x07': self.read_register,
+            b'\x08': self.new_section,
+            b'\x09': self.jump,
+            b'\x0a': self.jump_equal,
+            b'\x0b': self.jump_notequal,
+            b'\x0c': self.jump_less,
+            b'\x0d': self.jump_greater,
+            b'\x0e': self.jump_lessor,
+            b'\x0f': self.jump_greateror,
+            b'\x10': self.cycleStack,
             b'\x11': self.nul,
             b'\x12': self.nul,
             b'\x13': self.nul,
@@ -37,8 +38,7 @@ class VM(Frame):
             b'\x17': self.nul,
             b'\x18': self.nul,
             b'\x19': self.nul,
-            b'\x1a': self.nul,
-            b'\x1b': self.nul
+            b'\x1a': self.nul
         }
         self.registers = {
             'a': [None, None],
@@ -72,6 +72,8 @@ class VM(Frame):
         
         self.varibles = {}
         self.sections = {}
+        self.linepointer = 0
+        self.tline = None
         print("VM: Assembled")
 
     #configues window
@@ -140,8 +142,40 @@ class VM(Frame):
     #read register to the variable at the top of stack (7)
     def read_register(self, register):
         self.varibles[self.stack[0]] = self.registers[str(register[0].decode())][int(register[1])]
-    
-
+    #addes a new section to sections (8)
+    def new_section(self, sectionName, line):
+        self.sections[str(sectionName.decode())] = line
+    #jump to given section (9)
+    def jump(self, section):
+        self.linepointer = self.sections[str(section.decode())]
+    #jump to given section if j registers are equal (10)
+    def jump_equal(self, section):
+        if self.registers['j'][0] == self.registers['j'][1]:
+            self.linepointer = self.sections[str(section.decode())]
+    #jump to given section if j registers are not equal (11)
+    def jump_notequal(self, section):
+        if self.registers['j'][0] != self.registers['j'][1]:
+            self.linepointer = self.sections[str(section.decode())]
+    #jump to given section if j registers are less (12)
+    def jump_less(self, section):
+        if self.registers['j'][0] < self.registers['j'][1]:
+            self.linepointer = self.sections[str(section.decode())]
+    #jump to given section if j registers are greter (13)
+    def jump_greater(self, section):
+        if self.registers['j'][0] > self.registers['j'][1]:
+            self.linepointer = self.sections[str(section.decode())]
+    #jump to given section if j registers are less or equal (14)
+    def jump_lessor(self, section):
+        if self.registers['j'][0] <= self.registers['j'][1]:
+            self.linepointer = self.sections[str(section.decode())]
+    #jump to given section if j registers are greter or equal (15)
+    def jump_greateror(self, section):
+        if self.registers['j'][0] >= self.registers['j'][1]:
+            self.linepointer = self.sections[str(section.decode())]
+    #cycles first item in stack to the back of the stack (16)
+    def cycleStack(self):
+        self.stack.append(self.stack.pop(0))
+        
 
 root = Tk()
 root.geometry("400x300")
