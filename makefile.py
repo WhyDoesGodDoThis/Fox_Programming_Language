6c6eede210763dda1ef729308e63194e0e33f749
WhyDoesGodDoThis <phoenixnet.phoenix@gmail.com>
2022-07-15 22:39:43 -0400

    ermergency save before deleting debian for reinstall

--- makefile.py
+++ makefile.py
@@ -1,5 +1,10 @@
 #use this to turn main.foxbin into a print(text) program
-text = b"hello world!"
+text = 10.3
 with open("main.foxbin", "wb") as f:
-    bytes = bytearray([int(i) for i in bytearray(text)])
-    f.write(bytearray(b"\x08\x53\xff\x02\x61\xff\x04\x61\xff\x03")+bytes+bytearray(b"\xe2\xff\x06\x70\x00\xff\x01\xff\xfe"))
+    if text is float:
+        bytes = bytearray(bytes(text+b'\xe3'))
+    elif text is int:
+        bytes = bytearray(bytes(text)+b'\xe1')
+    else:
+        bytes = bytearray([int(i) for i in bytearray(text)]+[b'\xe2'])
+    f.write(bytearray(b"\x08\x53\xff\x02\x61\xff\x04\x61\xff\x03")+bytes+bytearray(b"\xff\x06\x70\x00\xff\x01\xff\xfe"))
